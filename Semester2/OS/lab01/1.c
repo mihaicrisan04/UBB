3bf0db707a448f15ba9b47d065e674b17bf3ab80
Mihai Crisan <crisanmihai2004@gmail.com>
2024-02-29 19:19:01 +0200

    lab 01

---
+++ Semester2/OS/lab01/1.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char *argv[] ) {
+	printf("argc = %d\n", argc);
+	for (int i = 0; i < argc; i++) {
+		printf("argv[%i] = %s\n",i, argv[i]);
+	}
+	printf("Hello world\n");
+	int **x = malloc(sizeof(int*) * 10);
+	for (int i = 0; i < 10; i++) {
+		x[i] = malloc(sizeof(int) * 10);
+	}
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			x[i][j] = i * j;
+		}
+	}
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			printf("%4d",  x[i][j]);
+		}
+		printf("\n");
+	}
+	
+	for (int i = 0; i < 10; i++) {
+		free(x[i]);
+	}
+	free(x);
+
+	return 0;
+}
