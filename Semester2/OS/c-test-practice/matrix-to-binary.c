91a9127a5c26112a559e5eb893f939ddf8186296
Mihai Crisan <crisanmihai2004@gmail.com>
2024-03-06 22:39:20 +0200

    solved 6

--- Semester2/OS/c-test-practice/matrix-to-binary.c
+++ Semester2/OS/c-test-practice/matrix-to-binary.c
@@ -1,40 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
     int** m;
     FILE* f;
-    int i, j, rows, cols, fd;
+    FILE* fd;
+    int i, j, rows, cols;
 
     f = fopen(argv[1], "r");
     if(f == NULL) {
         perror("Could not open file");
         return 1;
     }
-    fscanf(f, "%d %d", rows, &cols);
+    fscanf(f, "%d %d", &rows, &cols);
 
-    m = (int*)malloc(szeof(int*));
-    for(i=0 i<=rows; i++) {
-        m[i] = (int*)malloc(sizeof(int));
+    m = (int**)malloc(rows * sizeof(int*));
+    for(i=0; i<rows; i++) {
+        m[i] = (int*)malloc(cols * sizeof(int));
         for(j=0; j<cols; j++) {
-            fscanf(f, "%d", m[i][j]);
+            fscanf(f, "%d", &m[i][j]);
         }
     }
     fclose(f);
 
-    fd = open(argv[2], O_CREAT | O_WRONLY, 00600);
-    if(fd == -1) {
-        perror(Could not open destination file");
+    fd = fopen(argv[2], "wb"); 
+    if(fd == NULL) {
+        perror("Could not open destination file");
         return 1;
     }
-    write(fd, rows, sizeof(int));
-    write(fd, cols, sizeof(int));
+    fwrite(&rows, sizeof(int), 1, fd);
+    fwrite(&cols, sizeof(int), 1, fd);
     for(i=0; i<rows; i++) {
         for(j=0; j<cols; j++) {
-            write(fd, m[i][j], sizeof(int));
+            fwrite(&m[i][j], sizeof(int), 1, fd);
         }
     }
-    close(fd);
-
+    fclose(fd);
+    for (int i = 0; i < rows; i++) {
+        free(m[i]);
+    }
     free(m);
     return 0;
 }
