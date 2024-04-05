13e710927e830b72f9bcf449338d7526ea7e12f9
Mihai Crisan <crisanmihai2004@gmail.com>
2024-04-05 12:27:22 +0300

    added complexities

--- Semester2/DSA/Interfaces/SortedBag/SortedBag.h
+++ Semester2/DSA/Interfaces/SortedBag/SortedBag.h
@@ -27,27 +27,33 @@ public:
 	SortedBag(Relation r);
 
 	//adds an element to the sorted bag
+	// Complexity: O(n) - n is the number of elements in the sorted bag
 	void add(TComp e);
 
 	//removes one occurence of an element from a sorted bag
 	//returns true if an eleent was removed, false otherwise (if e was not part of the sorted bag)
+	// Complexity: O(n) - n is the number of elements in the sorted bag
 	bool remove(TComp e);
 
 	//checks if an element appearch is the sorted bag
+	// Complexity: O(n) - n is the number of elements in the sorted bag
 	bool search(TComp e) const;
 
 	//returns the number of occurrences for an element in the sorted bag
 	int nrOccurrences(TComp e) const;
 
 	//returns the number of elements from the sorted bag
+	// Complexity: Θ(1)
 	int size() const;
 
 	//returns an iterator for this sorted bag
 	SortedBagIterator iterator() const;
 
 	//checks if the sorted bag is empty
+	// Complexity: Θ(1)
 	bool isEmpty() const;
 
 	//destructor
+	// Complexity: O(n) - n is the number of elements in the sorted bag
 	~SortedBag();
 };
