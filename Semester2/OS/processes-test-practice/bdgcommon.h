3d69cac81f5dc34dc036758216ea758cc93e25b3
Mihai Crisan <crisanmihai2004@gmail.com>
2024-05-15 23:10:50 +0300

    a

---
+++ Semester2/OS/processes-test-practice/bdgcommon.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+
+
+#endif
+
