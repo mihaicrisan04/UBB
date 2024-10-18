ccd2cf7bbfde2b1fb9a01cdae88f55aa67e4b601
Mihai Crisan <crisanmihai2004@gmail.com>
2024-10-18 17:03:52 +0300

    x

--- Semester3/PLF/lab02/pb11.pl
+++ Semester3/PLF/lab02/pb11.pl
@@ -16,8 +16,9 @@ substitute([Head|Tail], X, Y, [Head|NewTail]) :-
 % Predicate to create a sublist from the main list.
 % sublist(+List-list, +Start-elem, +End-elem, -Sublist-list)(i, i, i, o)
 /*
-f(l1, l2, ..., ln, )
-
+f(l1, l2, ..., ln, start, end, s) = [], n = 0
+                                    f(l2, ..., ln, start - 1, end - 1, s), start > 1
+                                    l1 U f(l2, ..., ln, start - 1, end - 1, s), start = 1
 */
 sublist(List, Start, End, Sublist) :- 
     nth1(Start, List, _, Rest),          % Drop the elements before the start index.
@@ -25,14 +26,12 @@ sublist(List, Start, End, Sublist) :-
     prefix(Sublist, Rest),               % Take the first EndIndex elements.
     length(Sublist, EndIndex).           % Ensure the sublist is of the correct length.
 
-% Helper predicate: nth1/4 finds the element at the Start index and returns the remaining list.
 nth1(1, [H|T], H, T).                    % Base case, first element is H.
 nth1(N, [H|T], E, [H|Rest]) :-           % Recursive case, count down.
     N > 1,
     N1 is N - 1,
     nth1(N1, T, E, Rest).
 
-% Helper predicate: prefix/2 gets a prefix of a list.
 prefix([], _).                           % Base case, empty list is a prefix of any list.
 prefix([H|T], [H|Rest]) :-               % Recursive case, match head of both lists.
     prefix(T, Rest).
