6ef283c639d9a555346b49a728a44ec8f21c5d40
Mihai Crisan <crisanmihai2004@gmail.com>
2024-10-18 22:29:09 +0300

    lab2

--- Semester3/PLF/lab02/pb11.pl
+++ Semester3/PLF/lab02/pb11.pl
@@ -12,28 +12,29 @@ substitute([X|Tail], X, Y, [Y|NewTail]) :-
 substitute([Head|Tail], X, Y, [Head|NewTail]) :-  
     substitute(Tail, X, Y, NewTail). 
 
+% substitute([1, 2, 3, 2, 4], 2, 9, Result).
+
 
 % Predicate to create a sublist from the main list.
 % sublist(+List-list, +Start-elem, +End-elem, -Sublist-list)(i, i, i, o)
 /*
-f(l1, l2, ..., ln, start, end, s) = [], n = 0
-                                    f(l2, ..., ln, start - 1, end - 1, s), start > 1
-                                    l1 U f(l2, ..., ln, start - 1, end - 1, s), start = 1
+f(l1, l2, ..., ln, start, end, s) = [], start > end
+                                    f(l2, ..., ln, start - 1, end - 1, s), start > 0
+                                    l1 U f(l2, ..., ln, 0, end - 1, s), end >= 0
 */
-sublist(List, Start, End, Sublist) :- 
-    nth1(Start, List, _, Rest),          % Drop the elements before the start index.
-    EndIndex is End - Start + 1,         % Calculate the length of the sublist.
-    prefix(Sublist, Rest),               % Take the first EndIndex elements.
-    length(Sublist, EndIndex).           % Ensure the sublist is of the correct length.
+sublist(_, Start, End, []) :- 
+    Start > End.
 
-nth1(1, [H|T], H, T).                    % Base case, first element is H.
-nth1(N, [H|T], E, [H|Rest]) :-           % Recursive case, count down, until all elements before N are dropped.
-    N > 1,
-    N1 is N - 1,
-    nth1(N1, T, E, Rest).
+sublist([_|T], Start, End, Result) :- 
+    Start > 0, 
+    NewStart is Start - 1, 
+    NewEnd is End - 1, 
+    sublist(T, NewStart, NewEnd, Result).
 
-prefix([], _).                           % Base case, empty list is a prefix of any list.
-prefix([H|T], [H|Rest]) :-               % Recursive case, match head of both lists.
-    prefix(T, Rest).
+sublist([H|T], 0, End, [H|Result]) :- 
+    End >= 0, 
+    NewEnd is End - 1, 
+    sublist(T, 0, NewEnd, Result).
 
+% sublist([1,2,3,4,5,6],1,3,R).
 
