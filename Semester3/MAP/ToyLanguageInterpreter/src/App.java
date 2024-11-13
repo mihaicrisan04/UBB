e05585e2dbcf8811e5b8bdb331e073fa992c8550
Mihai Crisan <crisanmihai2004@gmail.com>
2024-11-13 12:40:06 +0200

    read

--- Semester3/MAP/ToyLanguageInterpreter/src/App.java
+++ Semester3/MAP/ToyLanguageInterpreter/src/App.java
@@ -1,6 +1,7 @@
 
 import model.expressions.*;
 import model.statements.*;
+import model.statements.files.*;
 import model.types.*;
 import model.values.*;
 import model.enums.*;
@@ -9,6 +10,7 @@ import view.*;
 
 public class App {
     public static void main(String[] args) {
+        // int v; v=2; Print(v)
         IStmt ex = new CompoundStmt(
             new VarDeclStmt("v", new IntType()),
             new CompoundStmt(
@@ -17,6 +19,7 @@ public class App {
             )
         );
 
+        // int a; int b; a=2+3; b=a+1; Print(b)
         IStmt ex2 = new CompoundStmt(
             new VarDeclStmt("v", new IntType()),
             new CompoundStmt(
@@ -33,6 +36,7 @@ public class App {
             )
         );
 
+        // int a; int b; a=2+3*5; b=a+1; Print(b)
         IStmt ex3 = new CompoundStmt(
             new VarDeclStmt("a", new IntType()),
             new CompoundStmt(
@@ -59,6 +63,7 @@ public class App {
             )
         );
 
+        // bool a; int v; a=true; (if a then v=2 else v=3); print(v)
         IStmt ex4 = new CompoundStmt(
             new VarDeclStmt("a", new BoolType()),
             new CompoundStmt(
@@ -72,6 +77,33 @@ public class App {
                             new AssignStmt("v", new ValueExp(new IntValue(3)))
                         ),
                         new PrintStmt(new VarExp("v"))
+                    )
+                )
+            )
+        );
+
+        // string varf; varf="test.in"; openRFile(varf); int varc; readFile(varf,varc);print(varc); readFile(varf,varc);print(varc) closeRFile(varf)
+        IStmt ex5 = new CompoundStmt(
+            new VarDeclStmt("varf", new StringType()),
+            new CompoundStmt(
+                new AssignStmt("varf", new ValueExp(new StringValue("test.in"))),
+                new CompoundStmt(
+                    new OpenStmt(new VarExp("varf")),
+                    new CompoundStmt(
+                        new VarDeclStmt("varc", new IntType()),
+                        new CompoundStmt(
+                            new ReadStmt(new VarExp("varf"), "varc"),
+                            new CompoundStmt(
+                                new PrintStmt(new VarExp("varc")),
+                                new CompoundStmt(
+                                    new ReadStmt(new VarExp("varf"), "varc"),
+                                    new CompoundStmt(
+                                        new PrintStmt(new VarExp("varc")),
+                                        new CloseStmt(new VarExp("varf"))
+                                    )
+                                )
+                            )
+                        )
                     )
                 )
             )
@@ -94,6 +126,7 @@ public class App {
         menu.addCommand(new RunExCommand("2", ex2.toString(), ex2));
         menu.addCommand(new RunExCommand("3", ex3.toString(), ex3));
         menu.addCommand(new RunExCommand("4", ex4.toString(), ex4));
+        menu.addCommand(new RunExCommand("5", ex5.toString(), ex5));
         menu.show();
     }
 }
--- Semester3/MAP/ToyLanguageInterpreter/src/model/statements/files/ReadStmt.java
+++ Semester3/MAP/ToyLanguageInterpreter/src/model/statements/files/ReadStmt.java
@@ -1,40 +1,76 @@
 package model.statements.files;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 import model.exceptions.MyException;
+import model.exceptions.StmtException;
 import model.statements.IStmt;
+import model.types.IntType;
+import collections.dictionary.MyIDictionary;
 import model.PrgState;
+import model.expressions.Exp;
+import model.values.Value;
+import model.values.StringValue;
+import model.types.StringType;
+import model.values.IntValue;
 
 public class ReadStmt implements IStmt {
+    private Exp exp;
     private String varName;
-    private String fileName;
 
-    public ReadStmt(String varName, String fileName) {
+    public ReadStmt(Exp exp, String varName) {
+        this.exp = exp;
         this.varName = varName;
-        this.fileName = fileName;
     }
 
-    public String getVarName() {
-        return varName;
-    }
+    public String getVarName() { return varName; }
 
-    public String getFileName() {
-        return fileName;
-    }
+    public Exp getExp() { return exp; }
 
     @Override
-    public PrgState execute(PrgState state) throws MyException{
-        // TODO: implement this
+    public PrgState execute(PrgState state) throws MyException {
+        MyIDictionary<String, Value> symTable = state.getSymTable();
+
+        if (!symTable.containsKey(varName)) { throw new StmtException("Variable not declared"); }
+        Value varVal = symTable.get(varName);
+        if (!varVal.getType().equals(new IntType())) { throw new StmtException("Variable is not an integer"); }
+
+        Value val;
+        try { val = exp.eval(symTable); }
+        catch (MyException e) { throw new StmtException("Error evaluating expression: " + e.getMessage()); }
+
+        if (!val.getType().equals(new StringType())) { throw new StmtException("Expression is not a string"); }
+
+        MyIDictionary<StringValue, BufferedReader> fileTable = state.getFileTable();
+        StringValue file = (StringValue) val;
+
+        if (!fileTable.containsKey(file)) { throw new StmtException("File is not opened"); }
+
+        BufferedReader reader = fileTable.get(file);
+        try {
+            String line = reader.readLine();
+            // if (line == null) { throw new StmtException("End of file reached"); }
+            if (line == null) {
+                symTable.put(varName, new IntValue(0));
+                return state;
+            }
+            int number = Integer.parseInt(line);
+            symTable.put(varName, new IntValue(number));
+        } catch (IOException e) {
+            throw new StmtException("Error reading from file");
+        }
 
         return state;
     }
 
     @Override
     public IStmt deepCopy() {
-        return new ReadStmt(varName, fileName);
+        return new ReadStmt(exp.deepCopy(), varName);
     }
 
     @Override
     public String toString() {
-        return "read(" + fileName + ", " + varName + ")";
+        return "read(" + exp.toString() + ", " + varName + ")";
     }
 }
