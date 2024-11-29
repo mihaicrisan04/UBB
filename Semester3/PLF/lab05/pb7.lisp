ad8f8eda4406e781ea0de5b8b8205b59036187d4
Mihai Crisan <crisanmihai2004@gmail.com>
2024-11-29 16:49:34 +0200

    pb7plf-l1

--- Semester3/PLF/lab05/pb7.lisp
+++ Semester3/PLF/lab05/pb7.lisp
@@ -29,3 +29,46 @@
             (cons (car lst) rest)))))
 
 (format t "~a~%" (successor '(1 9 3 5 9 9)))
+
+
+;; c)
+(defun flatten-list (lst)
+    (if (null lst)
+        nil
+        (if (listp (car lst))
+            (append (flatten-list (car lst)) (flatten-list (cdr lst)))
+            (cons (car lst) (flatten-list (cdr lst))))))
+
+(defun my-remove-duplicates (lst)
+    (if (null lst)
+        nil
+        (let ((rest (my-remove-duplicates (cdr lst))))
+         (if (member (car lst) rest)
+            rest
+            (cons (car lst) rest)))))
+
+(defun insert (element sorted-list)
+  (if (or (null sorted-list) (< element (car sorted-list)))
+      (cons element sorted-list)
+      (cons (car sorted-list) (insert element (cdr sorted-list)))))
+
+(defun insertion-sort (lst)
+  (if (null lst)
+      nil
+      (insert (car lst) (insertion-sort (cdr lst)))))
+
+(defun set-atoms (lst)
+  (insertion-sort (my-remove-duplicates (flatten-list lst))))
+
+(format t "~a~%" (set-atoms '(1 (2 (1 3 (2 4) 3) 1) (1 4))))
+
+;; d)
+(defun is-set (lst)
+    (if (null lst)
+        t
+        (if (member (car lst) (cdr lst))
+            nil
+            (is-set (cdr lst)))))
+
+(format t "~a~%" (is-set '(1 2 3 4 5 6 7 8 9 10)))
+(format t "~a~%" (is-set '(1 2 3 4 5 6 7 8 9 10 1)))
