cfbf300812c8c40804c934f3bd691bf14d23cfbb
Mihai Crisan <crisanmihai2004@gmail.com>
2024-11-29 15:59:48 +0200

    pb7plf_ab

---
+++ Semester3/PLF/lab05/pb7.lisp
@@ -0,0 +1,31 @@
+;; 7
+;; a) Write a function to eliminate the n-th element of a linear list.
+;; b) Write a function to determine the successor of a number represented digit by digit as a list, without
+;; transforming the representation of the number from list to number. Example: (1 9 3 5 9 9) --> (1 9 3 6 0
+;; 0)
+;; c) Write a function to return the set of all the atoms of a list.
+;;  Exemplu: (1 (2 (1 3 (2 4) 3) 1) (1 4)) ==> (1 2 3 4)
+;; d) Write a function to test whether a linear list is a set.
+
+;; a)
+(defun elim-nth (lst n)
+    (if (= n 1)
+        (cdr lst)
+        (cons (car lst) (elim-nth (cdr lst) (- n 1)))))
+
+(format t "~a~%" (elim-nth '(10 20 30 40 50) 3))
+
+;; b)
+(defun successor (lst)
+    (if (null (cdr lst))
+        (if (= (car lst) 9)
+            (cons 0 nil)
+            (cons (+ (car lst) 1) nil))
+        (let ((rest (successor (cdr lst))))
+         (if (= (car rest) 0)
+            (if (= (car lst) 9)
+                (cons 0 rest)
+                (cons (+ (car lst) 1) rest))
+            (cons (car lst) rest)))))
+
+(format t "~a~%" (successor '(1 9 3 5 9 9)))
