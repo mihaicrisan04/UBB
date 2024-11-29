9f51d65334f7ebd5b7dbb40a77e8660a9342526e
Mihai Crisan <crisanmihai2004@gmail.com>
2024-11-29 18:38:08 +0200

    lab05plf-done

--- Semester3/PLF/lab05/pb5.lisp
+++ Semester3/PLF/lab05/pb5.lisp
@@ -10,18 +10,24 @@
 ;; d) Write a function to return the number of all numerical atoms in a list at superficial level.
 
 
+
+
 ;; a) 
+;; twice-nth-element(l1, l2, ..., ln, n) = l1 U twice-nth-element(l2, ..., ln, n-1), n != 1
+;;                                         l1 U l1 U twice-nth-element(l2, ..., ln, n-1), n = 1
 (defun twice-nth-element (lst n)
   (if (null lst)
       nil
       (if (= n 1)
-          (cons (car lst) (cons (car lst) (twice-nth-element (cdr lst) n)))
+          (cons (car lst) (cons (car lst) (twice-nth-element (cdr lst) (- n 1))))
           (cons (car lst) (twice-nth-element (cdr lst) (- n 1))))))
 
 (format t "~a~%" (twice-nth-element '(10 20 30 40 50) 3))
 
 
 ;; b)
+;; assoc-list(l1, l2, ..., ln, m1, m2, ..., mn) = nil, if L = nil or M = nil
+;;                                                [l1, m1] U assoc-list(l2, ..., ln, m2, ..., mn)
 (defun assoc-list (lst1 lst2)
   (if (or (null lst1) (null lst2))
       nil
@@ -31,17 +37,22 @@
 
 
 ;; c)
+;; count-sublists(l1, l2, ..., ln) = 0, if L = nil
+;;                                   1 + count-sublists(l1) + count-sublists(l2, ..., ln), if l1 is a list
+;; count-sublists(l1, l2, ..., ln) + 1 to count the list itself
 (defun count-sublists (lst)
   (if (null lst)
       0
       (if (listp (car lst))
           (+ 1 (count-sublists (cdr lst)) (count-sublists (car lst)))
-          (+ 1 (count-sublists (cdr lst))))))
+          (count-sublists (cdr lst)))))
 
-(format t "~a~%" (count-sublists '(1 2 (3 (4 5) (6 7)) 8 (9 10))))
+(format t "~a~%" (+ 1 (count-sublists '(1 2 (3 (4 5) (6 7)) 8 (9 10)))))
 
 
 ;; d)
+;; count-numerical-atoms(l1, l2, ..., ln) = 0, if L = nil
+;;                                          1 + count-numerical-atoms(l2, ..., ln), if l1 is a number
 (defun count-numerical-atoms (lst)
   (if (null lst)
       0
@@ -51,3 +62,5 @@
 
 (format t "~a~%" (count-numerical-atoms '(1 2 (3 (4 5) (6 7)) 8 (9 10))))
 
+
+;; l2: 15
