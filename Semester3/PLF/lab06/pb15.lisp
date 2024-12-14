de92401fb2be35ee5a8c8eab767443bcbc9989f5
Mihai Crisan <crisanmihai2004@gmail.com>
2024-12-14 13:50:57 +0200

    plf-lab06

--- Semester3/PLF/lab06/pb15.lisp
+++ Semester3/PLF/lab06/pb15.lisp
@@ -14,22 +14,33 @@
 ;; d e    g h i
 ;; posotorder: d e b c g h i f a
 
+
+;; my-append(l1, l2, ..., ln, m1, m2, ...mn) = l1 + my-append(l2, ..., ln, m1, m2, ... mn), if l1 != null
+;;                                           = m1, m2, ..., mn, if l1 == null
+
 ;; posotorder(e, l1, ..., ln) = postorder-children(l2, ..., ln) + e, if l1 != nil
 ;;                            = e, if l1 = nil
 
 ;; posotorder-children(l1, ..., ln) = postorder(l1) + postorder-children(l2, ..., ln), if l1 != nil
 ;;                                   = nil, if l1 = nil
 
+(defun my-append (list1 list2)
+  (if (null list1)
+      list2
+      (cons (car list1) (my-append (cdr list1) list2))))
+
 (defun postorder (lst)
   (if (null lst)
       nil
-      (append (postorder-children (cdr lst))
-              (list (car lst)))))
+      (my-append (postorder-children (cdr lst))
+                 (list (car lst)))))
 
 (defun postorder-children (children)
   (if (null children)
       nil
-      (append (postorder (car children))
-              (postorder-children (cdr children)))))
+      (my-append (postorder (car children))
+                 (postorder-children (cdr children)))))
 
 (format t "~a~%" (postorder '(A (B) (C (D) (E)) (F (G) (H) (I))))) ; (B D E C G H I F A)
+(format t "~a~%" (postorder '(A (B) (C (D) (E))))) ; (B D E C A)
+
